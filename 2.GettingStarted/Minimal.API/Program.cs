afc0634790b34f5568e7030bf8998e975f51520e
TanerSaydam <tanersaydam@gmail.com>
2024-05-20 00:55:53 +0300

    The different return types

--- 2.GettingStarted/Minimal.API/Program.cs
+++ 2.GettingStarted/Minimal.API/Program.cs
@@ -4,4 +4,16 @@ var app = builder.Build();
 app.MapGet("get-example", () => "Hello from GET");
 app.MapPost("post-example", () => "Hello from POST");
 
+app.MapGet("ok-object", () => Results.Ok(new { Message = "API is working..." }));
+
+app.MapGet("slow-request", async () =>
+{
+    await Task.Delay(2000);
+
+    return Results.Ok(new
+    {
+        Message = "Slow API is working..."
+    });
+});
+
 app.Run();
