3ea27e16eae07a5448b5251b4a12c39a72efb455
TanerSaydam <tanersaydam@gmail.com>
2024-05-20 01:06:31 +0300

    Routing requests

---
+++ 2.GettingStarted/Minimal.API/Example.cs
@@ -0,0 +1,9 @@
+﻿namespace Minimal.API;
+
+public sealed class Example
+{
+    public static string SomeMethod()
+    {
+        return "This is coming from a some methods";
+    }
+}
--- 2.GettingStarted/Minimal.API/Program.cs
+++ 2.GettingStarted/Minimal.API/Program.cs
@@ -1,3 +1,5 @@
+using Minimal.API;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -15,5 +17,18 @@ app.MapGet("slow-request", async () =>
         Message = "Slow API is working..."
     });
 });
+
+
+app.MapGet("get", () => "This is a GET");
+app.MapPost("post", () => "This is a POST");
+app.MapPut("put", () => "This is a PUT");
+app.MapDelete("delete", () => "This is a DELETE");
+
+app.MapMethods("options-or-head", new[] { "HEAD", "OPTIONS" },
+    () => "Hello from either options or head");
+
+var handler = () => "This is coming from a var";
+app.MapGet("handler", handler);
+app.MapGet("fromclass", Example.SomeMethod);
 
 app.Run();
