8555f6ad0a5a5028b597d951235e9e355df7bd93
TanerSaydam <tanersaydam@gmail.com>
2024-05-20 00:49:00 +0300

    Creating new endpoints

--- 2.GettingStarted/Minimal.API/Program.cs
+++ 2.GettingStarted/Minimal.API/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.Urls.Add("https://0.0.0.0:7777");
-//app.Urls.Add("https://0.0.0.0:8888");
+app.MapGet("get-example", () => "Hello from GET");
+app.MapPost("post-example", () => "Hello from POST");
 
 app.Run();
