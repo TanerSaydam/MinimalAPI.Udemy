c22152ef884f724cbb68268aaf0727f7340ca221
TanerSaydam <tanersaydam@gmail.com>
2024-05-20 05:36:46 +0300

    Validation ekleme

--- 3.LetsBuild/Library.API/Program.cs
+++ 3.LetsBuild/Library.API/Program.cs
@@ -1,6 +1,8 @@
+using FluentValidation.Results;
 using Library.API.Context;
 using Library.API.Models;
 using Library.API.Services;
+using Library.API.Validators;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,10 +24,17 @@ app.UseSwaggerUI();
 
 app.MapPost("books", async (Book book, IBookService bookService) =>
 {
+    BookValidator validator = new();
+    ValidationResult validationResult = validator.Validate(book);
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors.Select(s => s.ErrorMessage));
+    }
+
     var result = await bookService.CreateAsync(book);
     if (!result) return Results.BadRequest("Something went wrong!");
 
-    return Results.Ok(result);
+    return Results.Ok(new { Message = "Book create is successful" });
 });
 
 app.Run();
---
+++ 3.LetsBuild/Library.API/Validators/BookValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Library.API.Models;
+
+namespace Library.API.Validators;
+
+public sealed class BookValidator : AbstractValidator<Book>
+{
+    public BookValidator()
+    {
+        RuleFor(p => p.Isbn).MinimumLength(13).MaximumLength(13);
+        RuleFor(p => p.Title).MinimumLength(3);
+        RuleFor(p => p.ShortDescription).MinimumLength(3);
+        RuleFor(p => p.PageCount).GreaterThan(10);
+    }
+}
