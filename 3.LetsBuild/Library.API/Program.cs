b39f03844bede75648521e8eed8e985b617049ee
TanerSaydam <tanersaydam@gmail.com>
2024-05-20 05:42:56 +0300

    Kitapları listeleme

--- 3.LetsBuild/Library.API/Program.cs
+++ 3.LetsBuild/Library.API/Program.cs
@@ -22,7 +22,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost("books", async (Book book, IBookService bookService) =>
+app.MapPost("books", async (Book book, IBookService bookService, CancellationToken cancellationToken) =>
 {
     BookValidator validator = new();
     ValidationResult validationResult = validator.Validate(book);
@@ -31,10 +31,16 @@ app.MapPost("books", async (Book book, IBookService bookService) =>
         return Results.BadRequest(validationResult.Errors.Select(s => s.ErrorMessage));
     }
 
-    var result = await bookService.CreateAsync(book);
+    var result = await bookService.CreateAsync(book, cancellationToken);
     if (!result) return Results.BadRequest("Something went wrong!");
 
     return Results.Ok(new { Message = "Book create is successful" });
+});
+
+app.MapGet("books", async (IBookService bookService, CancellationToken cancellationToken) =>
+{
+    var books = await bookService.GetAllAsync(cancellationToken);
+    return Results.Ok(books);
 });
 
 app.Run();
